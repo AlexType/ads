6c4b9d5f5ef6552f2465887d8e7a3c862ff5482c
Alexand Bryzgalov <bryzgalov@gnivc.ru>
2024-10-31 14:55:42 +1000

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-const resolveRoot = (...pathSegments: string[]) => path.resolve(process.cwd(), ...pathSegments);
+const resolveRoot = (...pathSegments: string[]) =>
+  path.resolve(process.cwd(), ...pathSegments);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/<repo>/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        includePaths: [resolveRoot('src', 'styles', 'common')],
+        includePaths: [resolveRoot("src", "styles", "common")],
       },
     },
   },
-})
+});
